409cc45e53e7350f87aa53ccf16083de561d5a5c
nechanie <104329653+nechanie@users.noreply.github.com>
2024-03-14 10:22:28 -0700

    web socket integration

--- nechanickyworks.client/src/pages/ProjectPage.jsx
+++ nechanickyworks.client/src/pages/ProjectPage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PageTitle from '../components/Shared/PageTitle';
+import PythonMLWebSocketTest from '../components/Demos/PythonMLWebsocketTest';
 
 const ProjectPage = () => {
     return (
         <div>
             <PageTitle pageTitle="Nechanicky Works" />
             <h1>Welcome to the Home Page</h1>
-            {/* Additional content here */}
+            <PythonMLWebSocketTest/>
         </div>
     );
 }
